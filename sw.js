fb5733410c70a94ec6234b24765ab0b4d34d2c9b
MD-ai-fin <may_dai@hotmail.com>
2026-07-27 22:31:17 -0500

    Fix title at the top

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-restricted-globals */
 /** Bump when shell assets change so clients refresh the cache. */
-const CACHE = "panda-itinerary-20260728k";
+const CACHE = "panda-itinerary-20260728l";
 
 const SHELL = [
   "./",
