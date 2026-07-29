087199fd1dfb4f9408419ddac64cca34e373a739
MD-ai-fin <may_dai@hotmail.com>
2026-07-29 01:08:35 -0500

    Update Dazu transportation means

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-restricted-globals */
 /** Bump when shell assets change so clients refresh the cache. */
-const CACHE = "panda-itinerary-20260731o";
+const CACHE = "panda-itinerary-20260731q";
 
 const SHELL = [
   "./",
